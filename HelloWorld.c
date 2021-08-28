6163a08254903aab50997b8fb3a601143dfdd1c0
Yong-joon Lee <yjl@etri.re.kr>
2021-08-28 23:36:46 +0900

    Update test files

---
+++ HelloWorld.c
@@ -0,0 +1,14 @@
+/*
+ * HelloWorld.c
+ *
+ *  Created on: 2019. 3. 15.
+ *      Author: YJL
+ */
+
+#include <stdio.h>
+int main()
+{
+	// printf() displays the string inside quotation
+	printf("Hello, World!");
+	return 0;
+}
